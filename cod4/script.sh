637a1100b52b3b89e7d1e9fd154deac7c3c3fad8
Henrik S Nielsen <1741035+henkall@users.noreply.github.com>
2018-03-05 16:36:01 +0100

    rearrange some files.

---
+++ cod4/script.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+if [ ! -f cod4x18_dedrun ]; then
+    echo "cod4x18_dedrun not found... trying to download it."
+    curl https://cod4x.me/downloads/cod4x_server-linux.zip > cod4x.zip && unzip -o cod4x.zip && rm cod4x.zip
+    echo "Download Done"
+    chmod +x cod4x18_dedrun
+    echo ready
+else
+  echo "cod4x18_dedrun found" 
+fi
+if [[ -f main ]]; then
+	cp xbase_00.iwd main/
+fi
+if [[ -z "${PORT}" ]]; then
+  echo "The PORT variable is empty."
+  PORT="28960"
+fi
+
+if [[ -z "${MAP}" ]]; then
+  echo "The MAP variable is empty."
+  MAP="+map_rotate"
+fi
+if [[ ! -z "${READY}" ]]; then
+./cod4x18_dedrun "+set net_port $PORT" "+set fs_game mods/$MODNAME" "$EXTRA" "+exec server.cfg" "$MAP"
+fi
