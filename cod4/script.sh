a9ce793cc1c7e5a67946b46299216e212ef3f114
Henrik S Nielsen <1741035+henkall@users.noreply.github.com>
2018-03-05 17:26:17 +0100

    Update script.sh

--- cod4/script.sh
+++ cod4/script.sh
@@ -9,14 +9,16 @@ else
   chmod +x cod4x18_dedrun
   echo "cod4x18_dedrun found" 
 fi
-if [[ -f main ]]; then
-	echo "main found"
-	if [[ ! -f main/xbase_00.iwd ]]; then
-	echo "xbase_00.iwd not found"
-	cp xbase_00.iwd main/
-	echo "copying xbase_00.iwd done"
-	fi
-fi
+#echo "Testing if main is there"
+#if [[ -f main ]]; then
+#	echo "main found"
+#	if [[ ! -f main/xbase_00.iwd ]]; then
+#	echo "xbase_00.iwd not found"
+#	cp xbase_00.iwd main/
+#	echo "copying xbase_00.iwd done"
+#	fi
+#fi
+#echo "Main not found or emty"
 if [[ -z "${PORT}" ]]; then
   echo "The PORT variable is empty."
   PORT="28960"
@@ -26,6 +28,12 @@ if [[ -z "${MAP}" ]]; then
   echo "The MAP variable is empty."
   MAP="+map_rotate"
 fi
+
 if [[ ! -z "${READY}" ]]; then
-./cod4x18_dedrun "+set net_port $PORT" "+set fs_game mods/$MODNAME" "$EXTRA" "+exec server.cfg" "$MAP"
+	if [[ ! -z "${MODNAME}" ]]; then
+		./cod4x18_dedrun "+set net_port $PORT" "+set fs_game mods/$MODNAME" "$EXTRA" "+exec server.cfg" "$MAP"
+	else
+		./cod4x18_dedrun "+set net_port $PORT" "$EXTRA" "+exec server.cfg" "$MAP"
+	fi
+
 fi
